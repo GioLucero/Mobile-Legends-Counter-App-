4a3f26fe4da7da19630241273fa2075ab5420ae8
GioLucero <gioandrewlucero@gmail.com>
2018-10-02 21:44:04 -0400

    Initial Commit

---
+++ Mobile Legends Counter App (Swift UI)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile Legends Counter App (Swift UI)
+//
+//  Created by Gio Lucero on 2018-10-02.
+//  Copyright © 2018 Gio Lucero. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
